a4af85d1a7eeed9eaf276966eb9f8c0e71f81b7b
Kshitij Gaur <kshitij@Kshitijs-MacBook-Pro.local>
2025-04-06 20:01:55 +0530

    interview session implement

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Agent from "@/components/Agent";
+import {getCurrentUser} from "@/lib/actions/auth.action";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const Page = async () => {
+    const user = await getCurrentUser();
+
+    return (
+        <>
+            <h3>Interview Generation</h3>
+
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
+        </>
+    )
 }
-
-export default page
+export default Page
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {Toaster} from "sonner";
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
